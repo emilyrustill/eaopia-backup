3bc3bda987915452397a81eb6990741381271f88
Angel <amu.maftouh@gmail.com>
2023-05-17 16:11:32 +0100

    a lot

--- scene switcher/Assets/Scripts/SceneSwitcher.cs
+++ scene switcher/Assets/Scripts/SceneSwitcher.cs
@@ -14,4 +14,9 @@ public class SceneSwitcher : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
